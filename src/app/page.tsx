490754f409e9240a1fc3d3ead99b88c991cf5d99
cestlvvie <167128213+cestlvvie@users.noreply.github.com>
2024-06-14 09:37:52 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Chat } from "@/components/chat";
+import Register from "@/components/register";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         
+         <Register />
     </main>
   );
 }
